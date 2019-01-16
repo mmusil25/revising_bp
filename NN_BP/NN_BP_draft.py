cf5cdc5c8d9e629e23078b3f83240dbcf21de696
Jian Meng <mengjian@pdx.edu>
2019-01-16 11:42:40 -0800

    Two layer classifier

---
+++ NN_BP/NN_BP_draft.py
@@ -0,0 +1,22 @@
+'''
+ECE Capstone Project 
+
+@author: Jian Meng
+'''
+
+import numpy as np
+import math
+
+# Define the activation function: 
+def sigmoid(activation):
+    output = 1.0 / (1.0 + math.exp(-activation))
+    return output
+
+def ReLU(activation):
+    pass
+# Calculate the linaer combination of the weights and the inputs
+def linear_combination(weights, inputs):
+    '''
+    
+    '''
+    pass
