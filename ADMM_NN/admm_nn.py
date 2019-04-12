c748c635cae87bbbc508c5f6e6d770712a344dac
naplings <naplings@outlook.com>
2019-04-12 15:39:26 -0700

    Minor Fixes

--- ADMM_NN/admm_nn.py
+++ ADMM_NN/admm_nn.py
@@ -40,9 +40,12 @@ class ADMM_Net(object):
         self.y_hat = None 
 
 
-    def train(self, X, y, n_epoch=500, shuffle=False, n_warm_start=10):
+    def train(self, X, y, n_epoch=500, n_warm_start=10):
         n_sample = X.shape[1]
-        n_turn = int(n_sample / self.n_batch) + 1
+        if n_sample % self.n_batch != 0:
+            n_turn = int(n_sample / self.n_batch) + 1
+        else:
+            n_turn = n_sample // self.n_batch
         X_ = [X[:, i : (i + self.n_batch)] for i in self.n_batch * np.array(range(n_turn))]
         y_ = [y[:, i : (i + self.n_batch)] for i in self.n_batch * np.array(range(n_turn))]
 
@@ -50,19 +53,46 @@ class ADMM_Net(object):
         ws_acc = []
         for _ in range(n_warm_start):
             for i in range(len(X_)):
-                self._update_without_lambda(y_[i])
+                self._a[0] = X_[i]
+                self._update(y_[i], with_lambda=False)
                 ws_acc.append(self._accuracy(y_[i], self.y_hat))
         print("Warm Start Finished. Accuracy: {}".format(sum(ws_acc)/len(ws_acc)))
 
         for ep in range(n_epoch):
             for i in range(len(X_)):
+                self._a[0] = X_[i]
                 self._update(y_[i])
-                print("Epoch: {}, Batch:{}, Accuracy: {}".format(ep, i, self._accuracy(y_[i], self.y_hat)))
-            
+                print("Epoch: {} - {}, Accuracy: {}".format(ep, i, self._accuracy(y_[i], self.y_hat)))
+
+
+    def warm_start(self, X, y, n_epoch=10):
+        n_sample = X.shape[1]
+        if n_sample % self.n_batch != 0:
+            n_turn = int(n_sample / self.n_batch) + 1
+        else:
+            n_turn = n_sample // self.n_batch
+        X_ = [X[:, i : (i + self.n_batch)] for i in self.n_batch * np.array(range(n_turn))]
+        y_ = [y[:, i : (i + self.n_batch)] for i in self.n_batch * np.array(range(n_turn))]
+
+        # Warm Start
+        ws_acc = []
+        for _ in range(n_epoch):
+            for i in range(len(X_)):
+                self._a[0] = X_[i]
+                self._update(y_[i], with_lambda=False)
+                ws_acc.append(self._accuracy(y_[i], self.y_hat))
+        print("Warm Start Finished. Accuracy: {}".format(sum(ws_acc)/len(ws_acc)))
+
 
     def predict(self, X):
-        return self._forward(X)
+        __a = []
+        __a.append(X)
+        __z = []
+        for i in range(len(self.W)):
+            __z.append(self.W[i].mm(__a[i]))
+            __a.append(self.activation(__z[i]))
 
+        return __z[-1]
 
     def _forward(self, X):
         self._a[0] = X
@@ -72,11 +102,11 @@ class ADMM_Net(object):
 
         self.y_hat = self._z[-1]
 
-        return self.y_hat
+        return self._z[-1]
 
 
 
-    def _update_without_lambda(self, y):
+    def _update(self, y, with_lambda=True):
         for i in range(self.n_layers - 1):
             self.W[i] = self._z[i].mm(torch.pinverse(self._a[i]))
 
@@ -88,21 +118,22 @@ class ADMM_Net(object):
                 ).mm(
                     self.beta[i + 1] * self.W[i + 1].t().mm(
                         self._z[i + 1]
-                    ) + self.gamma[i] * self._a[i + 1]
+                    ) + self.gamma[i] * self.activation(self._z[i])
                 )
             )
 
             self._z_update_hidden_layer_relu(i)
         
-        self.W[-1] = self._z[-1].mm(torch.pinverse(self._a[self.n_layers - 2]))
+        self.W[-1] = self._z[-1].mm(torch.pinverse(self._a[-2]))
         
-        alpha = self.W[-1].mm(self._a[self.n_layers - 2])
+        alpha = self.W[-1].mm(self._a[-2])
 
-        self._z[-1] = (self.beta[-1] * alpha + y - self._lambda) / (1 + self.beta[-1])
+        self._z[-1] = (self.beta[-1] * alpha + y + self._lambda) / (1 + self.beta[-1])
 
-    def _update(self, y):
-        self._update_without_lambda(y)
-        self._lambda = self._lambda + self.beta[-1] * (self._z[-1] - alpha) 
+        if with_lambda:
+            self._lambda = self._lambda + self.beta[-1] * (self._z[-1] - alpha) 
+
+        self.y_hat = self._z[-1]
 
 
     def _z_update_hidden_layer_relu(self, l):
@@ -111,11 +142,12 @@ class ADMM_Net(object):
         """
         alpha = self.W[l].mm(self._a[l])
 
-        opt_func = lambda z: torch.sum(
-                self.gamma[l] * ((self._a[l + 1] - self.activation(z)) ** 2) 
-                +
-                self.beta[l] * ((z - alpha) ** 2) 
-            )
+        opt_func = lambda z: (self.gamma[l] 
+            * ((self._a[l + 1] 
+            - (self.activation(z)) ** 2)) 
+            + (self.beta[l] 
+            * ((z - alpha) ** 2)))
+            
 
         # Case 1: z >= 0:
         r1 = (self.gamma[l] + self.beta[l] * alpha) / (self.gamma[l] + self.beta[l])
@@ -131,18 +163,19 @@ class ADMM_Net(object):
         min_decision = min_val1 < min_val2
 
         _z = torch.zeros_like(r1)
-        for i in range(len(_z)):
-            if min_decision[i]:
-                _z[i] = r1[i]
-            else:
-                _z[i] = r2[i]
+        for i in range(_z.shape[0]):
+            for j in range(_z.shape[1]):
+                if min_decision[i, j]:
+                    _z[i, j] = r1[i, j]
+                else:
+                    _z[i, j] = r2[i, j]
 
-        self._z[i] = _z
+        self._z[l] = _z
   
         
     def _accuracy(self, y, y_hat):
         tf = (torch.argmax(y, dim=0) == torch.argmax(y_hat, dim=0))
-        acc = torch.sum(tf) / len(tf)
+        acc = torch.sum(tf).float() / tf.shape[0]
 
         return acc
         
--- ADMM_NN/demo.py
+++ ADMM_NN/demo.py
@@ -1,6 +1,6 @@
 from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
 from admm_nn import ADMM_Net
-# from torch_mlp import torch_mlp_demo
 import torch
 import numpy as np  
 
@@ -9,11 +9,13 @@ data = load_iris()
 X = data['data']
 y = data['target']
 
-# torch_mlp_demo(X, y)
 
 y_one_hot = np.zeros((len(y), 3))
 y_one_hot[np.arange(len(y)), y] = 1
 
-admm_net = ADMM_Net(dim_in=4, dim_hidden=[32, 16], dim_out=3, n_batch=150)
+admm_net = ADMM_Net(dim_in=4, dim_hidden=[32, 16], dim_out=3, n_batch=150, beta=10.0, gamma=1.0)
 
-admm_net.train(torch.from_numpy(X).float(), torch.from_numpy(y_one_hot))
+admm_net.train(torch.from_numpy(X.T).float(), torch.from_numpy(y_one_hot.T).float())
+
+y_hat_oh = admm_net.predict(torch.from_numpy(X.T).float())
+y_hat = np.argmax(y_hat_oh, axis=0)
